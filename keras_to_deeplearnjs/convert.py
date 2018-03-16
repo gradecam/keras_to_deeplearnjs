3d29dd8a545a06e90191d7a788cfd3be80c99d24
Robert Porter <rob@gradecam.com>
2018-03-16 02:07:44 -0700

    misc cleanup

--- keras_to_deeplearnjs/convert.py
+++ keras_to_deeplearnjs/convert.py
@@ -4,32 +4,30 @@ from types import SimpleNamespace
 import json
 
 
-def saveWeightJSON(values):
-    vals =[round(x, 3) for x in  values.flatten().tolist()]
-    return "dl.tensor({values}, {shape})".format(values=json.dumps(vals), shape=json.dumps(values.shape))
-
-def saveWeightByteArray(values, byteArray):
-    begin = len(byteArray)
-    byteArray += values.flatten().astype('float32').tobytes()
-    end = len(byteArray)
-    return "dl.tensor(new Float32Array(weightBuf.slice({begin}, {end})), {shape})".format(begin=begin, end=end, shape=json.dumps(values.shape))
-
 class LayerConverter:
+    "Base class for all layer converters"
     def __init__(self, layer):
-        global math_ops
         self.layer = layer
         self.subs = {}
         self.subs['input'] = "layers['{name}']".format(name=layer.input.name)
-        if hasattr(self.__class__, 'attrs'):
-            for attr in self.__class__.attrs:
-                self.subs[attr] = json.dumps(getattr(layer, attr))
-
+        for attr in getattr(self.__class__, 'attrs', []):
+            self.subs[attr] = json.dumps(getattr(layer, attr))
 
     def format_op(self, op):
         return op.format(**self.subs)
 
     def get_output_name(self):
         return self.layer.output.name
+
+    def _save_weight_json(self, values):
+        vals =[round(x, 3) for x in  values.flatten().tolist()]
+        return "dl.tensor({values}, {shape})".format(values=json.dumps(vals), shape=json.dumps(values.shape))
+
+    def _save_weight_bytearray(self, values, byteArray):
+        begin = len(byteArray)
+        byteArray += values.flatten().astype('float32').tobytes()
+        end = len(byteArray)
+        return "dl.tensor(new Float32Array(weightBuf.slice({begin}, {end})), {shape})".format(begin=begin, end=end, shape=json.dumps(values.shape))
 
     def get_deeplearn_weights(self, byteArray = None):
         "Save the weights for deeplearn.js.  If byteArray is passed it is assumed that we are not inlining the weights"
@@ -45,8 +43,8 @@ class LayerConverter:
             weight = getattr(self.layer, attr)
             name = weight.name
             values=K.eval(weight)
-            if byteArray is not None: weights[name] = saveWeightByteArray(values, byteArray)
-            else: weights[name] = saveWeightJSON(values)
+            if byteArray is not None: weights[name] = self._save_weight_bytearray(values, byteArray)
+            else: weights[name] = self._save_weight_json(values)
             self.subs[attr] = "weights['{name}']".format(name=name)
         return weights
 
